2a5b3ad2c4775cdc0d0ef7c3199088e034e1e414
Ivan2nn <i.bernabucci@gmail.com>
2019-07-29 18:37:22 +0200

    Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -18,15 +19,28 @@ class NewVisitorTest(unittest.TestCase):
 
 		# she notices the page title and the header mentions the to-do list
 		self.assertIn('To-Do', self.browser.title)
-		self.fail("Finish the test")
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		# She is invited to enter a todo item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'), 'Enter a to-do item'
+			)
 
 		# She types "Buy a new laptop"  into the box text
+		inputbox.send_keys("Buy a new laptop")
 
 		# when she hits enter the page refresh, updates and now the pag elist: "1 - Buy a new laptop"
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy a new laptop' for row in rows))
 
 		# There is anothe rtextbox inviting her to insert another item. she enters "Use the laptop to make design"
+		self.fail('Finish the test!')
 
 		# the page updates again and now shows both the items of the list
 
