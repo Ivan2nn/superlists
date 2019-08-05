2589e0fe4d10df1db4457f62976a932d1db2cdd1
Ivan2nn <i.bernabucci@gmail.com>
2019-08-05 09:47:05 +0200

    new URL, view template to to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -28,27 +28,6 @@ class HomePageTest(TestCase):
 
 		self.assertEqual(response.content.decode(), expected_html)
 
-	def test_home_page_can_save_a_POST_request(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
-
-		response = home_page(request)
-
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual('A new list item', new_item.text)
-
-	def test_home_page_redircts_after_POST(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
-
-		response = home_page(request)
-
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
-
 
 class ItemModelTest(TestCase):
 
@@ -90,3 +69,19 @@ class ListViewTest(TestCase):
 
 		self.assertContains(response, 'itemey1')
 		self.assertContains(response, 'itemey2')
+
+
+class NewListTest(TestCase):
+
+	def test_saving_a_POST_request(self):
+		self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual('A new list item', new_item.text)
+
+	def test_redirects_after_a_POST(self):
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
